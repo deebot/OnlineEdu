ebfc11c9303d170d53599e3098d869ace04af8d8
Deepankar <deepanakrflaseemail@gmail.com>
2021-01-16 05:22:47 +0100

    Improved the implementation of Composite design Pattern to Create Frames

--- view.py
+++ view.py
@@ -26,20 +26,28 @@ class Composite(Component):
     def __init__(self):
         Component.__init__(self)
         self._children = []
-        self.entryValue=None
 
     def getChild(self, index):
         return self._children[index]
 
     def add(self, component):
         self._children.append(component)
-        print("Added a new component"+ str(component))
+        print("Added new element")
 
     def remove(self, index):
         self._children.remove(index)
 
-    def operation(self,index):
-        self._children[index].operation()
+    def operation(self,i):
+        #for i in range(len(self._children)):
+        #print(self._children[i])
+        return self._children[i].operation()
+
+
+    def setEntryValue(self,i,x):
+         self._children[i].setEntryValue(x)
+
+    def getEntryValue(self,i):
+        return self._children[i].getEntryValue()
 
 
 #
@@ -47,72 +55,66 @@ class Composite(Component):
 # defines the behavior for the elements in the composition,
 # it has no children
 #
-class ButtonLeaf(Component):
-    def __init__(self, index):
+class LeafButton(Component):
+    def __init__(self, alias):
         Component.__init__(self)
-        self._idx = index
+        self._alias = alias
 
     def operation(self):
-        return "Button" + str(self._idx)
-
-class EntryLeaf(Component):
-    def __init__(self, index):
-        Component.__init__(self)
-        self._idx = index
-        self._entrydata="d"
-
-    def operation(self):
-        print("EntryLeaf " + str(self._idx) + " operation.")
-
-    def setEntryValue(self, edata):
-        self._entrydata = edata
+        #print("Leaf----button" + str(self._alias))
+        return "button" + str(self._alias)
 
     def getEntryValue(self):
-        return self._entrydata
+        pass
 
-
-
-class LabelLeaf(Component):
-    def __init__(self, index):
+class LeafEntry(Component):
+    def __init__(self, alias):
         Component.__init__(self)
-        self._idx = index
+        self._alias = alias
+        self._Evalue="apple"
 
     def operation(self):
-        print("LabelLeaf " + str(self._idx) + " operation.")
-        return "Button"+str(self._idx)
+        #print("entry" + str(self._alias))
+        return "entry" + str(self._alias)
 
+    def setEntryValue(self,val):
+        self._Evalue=val
+        print("Value set to " + str(self._Evalue))
 
+    def getEntryValue(self):
 
-class View():
+        #print("Value is " + str(self._Evalue))
+        return self._Evalue
+
+class View(Composite):
 
     def __init__(self,controller):
         self.controller=controller
+        Composite.__init__(self)
+        self.LoginFrame=Composite()
+
+    def attachButtonClick(self,x):
+        self.controller.onButtonClick(x.operation())
 
     def loginPage(self):
-        LoginFrame = Composite()
-        print("----------Composing Login Screen ---------------------------------")
-        UserID = EntryLeaf(1)
-        Password = EntryLeaf(2)
-        SubmitButton = ButtonLeaf(1)
-        LoginFrame.add(UserID)
-        LoginFrame.add(Password)
-        LoginFrame.add(SubmitButton)
-        self.controller.onButtonClick(SubmitButton.operation())
-
-        print("------------------End----------------------------------------")
-        UserID.setEntryValue("Deepankar")
-        Password.setEntryValue("123456")
+        print("-----------------------Composing Login Screen ---------------------------------")
+        self.LoginFrame.add(LeafEntry("UserID"))
+        self.LoginFrame.add(LeafEntry("Password"))
+        self.LoginFrame.add(LeafButton("Submit"))
+        print("----------------------- Login Screen Composition Done---------------------------")
+        print("------------------------Supply Inputs to Login---------------------------------")
+        self.LoginFrame.setEntryValue(0,(str(input('Enter the UserID '))))
+        self.LoginFrame.setEntryValue(1, (str(input('Enter the Password'))))
+        simulate_ButtonPress= str(input("Press Submit Button (y/n)"))
+        if simulate_ButtonPress =="y":
+            self.controller.onButtonClick(self.LoginFrame.operation(2))
 
     def profilePage(self):
-        ProfileFrame =Composite()
-        print("---------Composing Profile Screen-----------------------------")
-
-
+       # ProfileFrame =Composite()
+        print("------------------------Composing Profile Screen-----------------------------")
 
     def VideoConferencePage(self):
         pass
-
-
 
 if __name__ == "__main__":
     pass
